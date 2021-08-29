7ed4b0aeff988945819e3905f44286ddf48d17b1
chengruixin <chengruixin_2010@163.com>
2021-08-29 22:51:32 +0800

    add call and apply

--- nodeenv/handwrite/call.js
+++ nodeenv/handwrite/call.js
@@ -1,36 +1,34 @@
-Function.prototype.call1 = function (context, ...args) {
-    const fn = this;
+var global;
 
+(function () {
+    global = this;
+})();
+
+Function.prototype.myCall = function (context, ...args) {
+    const fn = this;
+    context = !context ? global : context;
     context._fn = fn;
 
     const res = context._fn(...args);
 
     context._fn = undefined;
-    // delete context._fn;
+    delete context._fn;
 
     return res;
 };
 
-Function.prototype.myCall = function (context) {
-    context.fn = this;
-    let params = "";
-    for (let i = 1; i < arguments.length; i++) {
-        params += `arguments[${i}]`;
-        if (i != arguments.length - 1) params += ",";
-    }
-    let result = eval("context.fn(" + params + ");");
-
-    delete context.fn;
-
-    return result;
-};
-
-const a = {
-    a: 1,
-};
-
 function show() {
-    console.log(this);
+    console.log(this.name);
 }
 
-show.call({ b: 1 });
+let a = {
+    name: 1,
+};
+
+let b = {
+    name: 2,
+};
+
+show.call(a);
+show.call(b);
+show.call();
